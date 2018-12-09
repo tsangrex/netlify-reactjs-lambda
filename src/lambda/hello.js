d6d7015d6e26db3754147a7f56d4bd7028f164cc
ReCrux <tsangchiho@hotmail.com>
2018-12-09 16:19:21 +0800

    test

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -5,6 +5,9 @@ const obj = {
   foo: process.env.MONGODB_URL
 };
 export function handler(event, context, callback) {
+  console.log(process);
+  console.log(process.env);
+  console.log(process.env.MONGODB_URL);
   console.log("queryStringParameters", event.queryStringParameters);
   callback(null, {
     statusCode: 200,
