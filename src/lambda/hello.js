091aa5cb5f68d8f8715af1a3575932c3d11ff148
ReCrux <tsangchiho@hotmail.com>
2018-12-09 16:57:57 +0800

    test

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,29 +1,37 @@
 // show object spread works, i.e. babel works
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {useNewUrlParser: true}
+);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const obj = {
   foo: "test"
 };
+console.log("db connected");
 const AccessLog = require("./AccessLog");
 export function handler(event, context, callback) {
   let accessLog = new AccessLog({
     event: event,
     context: context
   });
+  console.log("before save");
   accessLog.save(function(err) {
     if (err) {
+      console.log("save error");
       callback(null, {
         statusCode: 500,
         body: JSON.stringify({msg: "failed to create", ...obj})
       });
     } else {
+      console.log("save success");
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({msg: "create success", ...obj})
       });
     }
   });
+  console.log("after save");
 }
