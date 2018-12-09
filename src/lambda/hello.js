5d2dd7faae35302c6ca6db38304a437c3b08b0fb
ReCrux <tsangchiho@hotmail.com>
2018-12-09 16:09:50 +0800

    test

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,13 @@
 // show object spread works, i.e. babel works
+const mongoose = require("mongoose");
+
 const obj = {
-  foo: 'bar'
+  foo: process.env.MONGODB_URL
 };
 export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters);
+  console.log("queryStringParameters", event.queryStringParameters);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
+    body: JSON.stringify({msg: "Hello, World!", ...obj})
   });
 }
